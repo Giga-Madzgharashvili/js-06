1487a5ae494175d3ed68211f5e3922a89bd7e195
giga <gmadzgharashvili@gmail.com>
2022-09-17 00:06:05 +0400

    ...

---
+++ js/app.js
@@ -0,0 +1,32 @@
+
+function getData(){
+    let request = new XMLHttpRequest();
+
+    function returnData(){
+        let returnRequest = this.responseText;
+        let newData = JSON.parse(returnRequest);
+        let ul = document.createElement("ul");
+        ul.classList.add("ul-data");
+        newData.data.forEach((element) => {
+            let li = document.createElement("li");
+            li.textContent = element.first_name ;
+            let img = document.createElement("img");
+            img.setAttribute("src", element.avatar);
+            img.classList.add("images");
+            ul.appendChild(li);
+            li.appendChild(img);
+            
+            
+          });
+        document.getElementById("app").appendChild(ul);
+  
+        
+       
+    }
+    request.addEventListener("load", returnData);
+    request.open("GET", "https://reqres.in/api/users?page=2");
+    request.send();
+}
+
+getData();
+
