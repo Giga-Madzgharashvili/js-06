8fee9ad52c604c86a66c75f0c601a3bdfc7335bd
giga <gmadzgharashvili@gmail.com>
2022-09-18 05:54:24 +0400

    ...

--- js/app.js
+++ js/app.js
@@ -4,16 +4,21 @@ function getData(){
 
     function returnData(){
         let returnRequest = this.responseText;
+       
         let newData = JSON.parse(returnRequest);
         let ul = document.createElement("ul");
         ul.classList.add("ul-data");
         newData.data.forEach((element) => {
             let li = document.createElement("li");
-            li.textContent = element.first_name ;
+            li.classList.add("app-li");
+            let p = document.createElement("p");
+            p.classList.add("title");
+            p.textContent = element.first_name ;
             let img = document.createElement("img");
             img.setAttribute("src", element.avatar);
             img.classList.add("images");
             ul.appendChild(li);
+            li.appendChild(p);
             li.appendChild(img);
             
             
---
+++ js/main.js
@@ -0,0 +1,49 @@
+const sites = {
+  site1: {
+    siteName: "https://reqres.in/api/users?page=2",
+    returnData: function () {
+      return this.siteName;
+    },
+  },
+  site2: {
+    siteName: "https://reqres.in/api/unknown",
+    returnData: function () {
+      return this.siteName;
+    },
+  },
+};
+function getData(){
+    let request = new XMLHttpRequest();
+
+    function returnData(){
+        let returnRequest = this.responseText;
+        let newData = JSON.parse(returnRequest);
+        let ul = document.createElement("ul");
+        ul.classList.add("data");
+        newData.data.forEach((element) => {
+            let li = document.createElement("li");
+            li.classList.add("data-li");
+            let div = document.createElement("div");
+            div.classList.add("data-title");
+            div.textContent = element.name ;
+            let p = document.createElement("p");
+            p.classList.add("data-p");
+            p.textContent =element.year;
+            ul.appendChild(li);
+            li.appendChild(div);
+            li.appendChild(p);
+            
+            
+            
+          });
+        document.getElementById("data").appendChild(ul);
+  
+        
+       
+    }
+    request.addEventListener("load", returnData);
+    request.open("GET", "https://reqres.in/api/unknown");
+    request.send();
+}
+
+getData();
